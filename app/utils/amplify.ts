fcc335007888192cae516fd7e2d22c4d62a93e4e
nranganath <nranganath@cargurus.com>
2025-02-22 17:55:01 -0500

    more clean up

--- app/utils/amplify.ts
+++
@@ -1,7 +0,0 @@
-import { Amplify } from 'aws-amplify';
-import config from '@aws-amplify/backend/exported-backend-config.json';
-
-// Configure Amplify in your app
-Amplify.configure(config);
-
-export { Amplify }; 
